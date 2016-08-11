95cc25b4a88cc95fa1297323de21dd81b0a5a4c8
Nicholas Palenchar <nick@Nicks-pro.local>
2016-08-11 14:58:06 -0400

    continue with unique name generation

--- api/rooms/rooms.controller.js
+++ api/rooms/rooms.controller.js
@@ -4,13 +4,25 @@ var Rooms = require('mongoose').model('Room')
 // gets all rooms of the same id (from ip address) for user to join. Or creates the first one.
 module.exports.findOrCreate = function(req, res){
 
-  Rooms.find({ id: req.params.id })
-    .then(result => {
-      if(result) {
-
-      }
-    });
+  let theStatus;
 
   console.log("hellooooo youve hit this route!!!", req.ip);
-  res.status(200).send(req.ip);
-}
+
+  Rooms.find({ id: req.params.id })
+    .then(result => {
+      console.log("[rooms.controller] the result: ", result);
+      if(result) {
+        theStatus = 302;
+        return result
+      }
+      else {
+        theStatus(201);
+        return Rooms.create({
+          id: req.params.id
+        })
+      }
+    })
+    .then(rooms => {
+      req.states(theStatus).send(rooms);
+    });
+};
--- db/rooms/room.js
+++ db/rooms/room.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+var generateName = require('../../helpers/words');
+
 var RoomSchema = new Schema({
   id: String,
   name: String,
@@ -10,4 +12,17 @@ RoomSchema.methods.delete = function () {
   return this.remove();
 };
 
+RoomSchema.pre('save', function (next) {
+  if(this.name) return next();
+  let _n;
+  let tryName = (function (n) {
+    let name = generateName(n||"");
+    this.constructor.find({id: this.id, name: name})
+      .then(result => {
+        if(result && result.length) return tryName((n||0) + 1);
+
+      })
+  }).bind(this);
+
+});
 mongoose.model('Room', RoomSchema);
