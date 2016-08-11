70cf867fd2b3fc80bc8be4ec0c11a09d13305a5c
Nicholas Palenchar <nick@Nicks-pro.local>
2016-08-11 14:37:19 -0400

    helper fn for room model

--- api/rooms/rooms.controller.js
+++ api/rooms/rooms.controller.js
@@ -1,8 +1,16 @@
+var Rooms = require('mongoose').model('Room')
 
-
-/// URL: /api/rooms/findOrCreate/:id
+/// URL: /api/rooms/find/:id
 // gets all rooms of the same id (from ip address) for user to join. Or creates the first one.
 module.exports.findOrCreate = function(req, res){
-  console.log("hellooooo youve hit this route!!!");
-  res.status(200).send();
+
+  Rooms.find({ id: req.params.id })
+    .then(result => {
+      if(result) {
+
+      }
+    });
+
+  console.log("hellooooo youve hit this route!!!", req.ip);
+  res.status(200).send(req.ip);
 }
---
+++ helpers/words.js
@@ -0,0 +1,25 @@
+const ADJ = [
+  "immense",
+  "epic",
+  "notorious",
+  "evil",
+  "vast",
+  "noble",
+  "holy",
+  "magestic",
+];
+const NOUN = [
+  "badlands",
+  "road",
+  "sword",
+  "wall",
+  "king's road"
+];
+
+function generateName(n="") {
+  return ADJ[Math.floor(Math.random() * ADJ.length)]
+          + NOUN[Math.floor(Math.random() * NOUN.length)]
+          + n;
+}
+
+module.exports = generateName;
