763556d3e2d1c5344e6d0cc2253deae92e9e7ce1
Nicholas Palenchar <nick@Nicks-pro.local>
2016-08-19 17:57:36 -0400

    it works

--- io/index.js
+++ io/index.js
@@ -15,49 +15,49 @@ module.exports = function (server) {
 
   io = socketio(server);
 
-  io.on('connection', function (io) {
+  io.on('connection', function (socket) {
     console.log("connecteddddd");
+    io.sockets.emit("test", 'to all someone ocnnected')
 
-    io.on('join-room', function (room) {
+    socket.on('join-room', function (room) {
       console.log(chalk.yellow("request to join room " + room));
       if(typeof room !== 'string') {
         console.log('need to join a room by string. returnin');
         return;
       }
-      io.join(room);
-      io.emit('test', 'standard test');
-      io.to("nonexistantroom").emit("err", "you should not have gotten this");
-      console.log("THE ROOML?", io.room);
+      socket.join(room);
+      socket.emit('test', 'standard test');
+      io.sockets.emit('test', 'FROM ALL');
+      io.sockets.to(room).emit('test', "THIS IS THE ROOOM");
+      socket.to("nonexistantroom").emit("err", "you should not have gotten this");
+      console.log("THE ROOML?", socket.room);
     });
 
-    io.on('send-test', function () {
+    socket.on('send-test', function () {
       console.log("test reciewen");
-      io.emit('test', 'this is a test');
+      socket.emit('test', 'this is a test');
     })
-    io.on('test-room', function(room){
+    socket.on('test-room', function(room){
       console.log("test recieved??? in room??", room);
-      io.in(room).emit('message', 'this went to room ' + room);
+      io.sockets.in(room).emit('message', 'this went to room ' + room);
     })
 
     //// all sockets should have the roomId as the first arg so that it will only be broadcasted to that room
     //// rooms _id used for each room.
 
-    io.on("update-players", function (room, players) {
-      io.in(room).emit("update-players", players);
+    socket.on("update-players", function (room, players) {
+      socket.in(room).emit("update-players", players);
     });
 
-    io.on("remove-player", function (room, player) {
+    socket.on("remove-player", function (room, player) {
       console.log("teste workeddd")
     });
 
-    io.on('disconnect',function () {
+    socket.on('disconnect',function () {
       console.log("discennected");
     })
   });
 
-  io.on("me", function () {
-    console.log("OUTSIDE");
-  })
 
   return io;
 
